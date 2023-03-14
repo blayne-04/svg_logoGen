ee887f93e9ed8d6da50ac2e61e08e780a4c89e93
blayne-04 <befuller2004@gmail.com>
2023-03-14 16:00:19 -0700

    added switch statement

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+const fs = require('fs')
 const questions = require('./lib/userInput')// including questions for prompt
 const {Shape} = require('./lib/shapes')// inlcuding constructors
 const inquirer = require('inquirer')//including inquirer
 
 inquirer.prompt(questions)
 .then((res) =>{ 
-const shape = new Shape(res.text, res.textColor, res.logoColor)
-console.log(shape);
+    switch(res.shape){
+        case 'Circle':
+            fs.writeFile(`circle.svg`,`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+            <circle cx="150" cy="100" r="80" fill="${res.logoColor}" />
+          
+            <text x="150" y="125" font-size="60" text-anchor="middle" fill="${res.textColor}">${res.text}</text>
+          
+          </svg>`, (err) => {
+            if(err){
+                console.log(err)
+            }
+            })
+        }
 })
