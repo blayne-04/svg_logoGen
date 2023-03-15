3eefc273977852ec5ff4747f71ab76f8281ca795
blayne-04 <befuller2004@gmail.com>
2023-03-14 21:04:56 -0700

    finished shape render functionality, finished jest tests

--- README.md
+++ README.md
@@ -1,5 +1,43 @@
-# svg_logoGen
-Generates SVG circle, triangle, square
+# SVG Logo Generator
+Unlicensed
+## Table Of Contents
+  1. [Description](#description)
+  2. [Installation](#installation)
+  3. [Usage](#usage)
+  4. [Contributions](#contributions)
+  5. [Testing](#testing)
+  6. [License](#license)
+  7. [Questions](#questions)
+  8. [Resources](#resources)
+  9. [Tutorial](#tutorial)
+## Description 
+`Generates an SVG file containing a circle, triangle, or square, with different colors and text based on user input`
+
+## Installation
+`N/A`
+
+## Usage
+`Open the CLI, enter npm i, then enter node index.js, answer the prompts, once all prompts are answered a shape with text will be generated based on your input!`
+
+## Contributions
+`Blayne Fuller`
+
+## Testing
+`type npm test in the CLI after running npm i`
+
+## License
+- [License Documentation](https://choosealicense.com/no-permission/)
+
+## Questions
+- [Github Profile](https://github.com/blayne-04)
+- If you can't find the answer to your questions on my github profile feel free to contact me at befuller2004@gmail.com
 
 # Resources
-[List of all SVG color keywords](https://upload.wikimedia.org/wikipedia/commons/2/2b/SVG_Recognized_color_keyword_names.svg)
+- [List of all SVG color keywords](https://upload.wikimedia.org/wikipedia/commons/2/2b/SVG_Recognized_color_keyword_names.svg)
+- [SVG Tutorial](https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial)
+- [Basic Shapes With SVG](https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Basic_Shapes)
+- [Regex for Hex Codes](https://stackoverflow.com/questions/8027423/how-to-check-if-a-string-is-a-valid-hex-color-representation)
+- [Switch Statements](https://www.w3schools.com/js/js_switch.asp)
+- [Javascript Classes](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes)
+
+## Tutorial
--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 const fs = require('fs')
 const questions = require('./lib/userInput')// including questions for prompt
-const {Shape} = require('./lib/shapes')// inlcuding constructors
+const {Circle, Triangle, Square} = require('./lib/shapes')// inlcuding constructors
 const inquirer = require('inquirer')//including inquirer
 
 inquirer.prompt(questions)
 .then((res) =>{ 
     switch(res.shape){
         case 'Circle':
-            fs.writeFile(`circle.svg`,`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-
-            <circle cx="150" cy="100" r="80" fill="${res.logoColor}" />
-          
-            <text x="150" y="125" font-size="60" text-anchor="middle" fill="${res.textColor}">${res.text}</text>
-          
-          </svg>`, (err) => {
-            if(err){
-                console.log(err)
-            }
+            const circle = new Circle(res.text, res.textColor, res.logoColor)
+                fs.writeFile(`examples/${res.fileName}.svg`, circle.render, (err) => {
+                    err ? console.log(err) : console.log(`Created ${res.fileName}.svg!`)
+            });
+            break;
+        case 'Triangle':
+            const triangle = new Triangle(res.text, res.textColor, res.logoColor)
+                fs.writeFile(`examples/${res.fileName}.svg`, triangle.render, (err) => {
+                    err ? console.log(err) : console.log(`Created ${res.fileName}.svg!`)
             })
-        }
+            break;
+        case 'Square':
+            const square = new Square(res.text, res.textColor, res.logoColor)
+                fs.writeFile(`examples/${res.fileName}.svg`, square.render, (err) => {
+                    err ? console.log(err) : console.log(`Created ${res.fileName}.svg!`)
+            })
+            break;
+        default: console.log('No parameters given')
+    }
 })
